68af813ce3dfdf3ec0401f50f269e9c1729c7712
DanielVerloop <a.c.verloop@student.tue.nl>
2022-03-28 12:36:57 +0200

    wip
    finished given prep

--- Generate/generate.py
+++ Generate/generate.py
@@ -50,33 +50,58 @@ def get_pos(sentence):
     return result
 
 
+def extract_nouns_and_values(pos_tags):
+    nouns = []
+    values = []
+    for word in range(0, len(pos_tags)):
+        if pos_tags[word][1] == "NN":
+            if word + 1 < len(pos_tags): #out-of-bound protection
+                if pos_tags[word + 1][1] == "NN":
+                    nouns += [pos_tags[word][0] + " " + pos_tags[word + 1][0]]
+                    word += 1
+                else:
+                    nouns += [pos_tags[word][0]]
+            else:
+                nouns += [pos_tags[word][0]]
+        if pos_tags[word][1] == "CD":
+            values += [pos_tags[word][0]]
+    if (pos_tags[len(pos_tags) - 1][1] == "NN") and pos_tags[len(pos_tags) - 1][0] not in nouns:
+        nouns += [pos_tags[len(pos_tags) - 1][0]]
+
+    return nouns, values
+
+
+def extract_verbs(pos_tags):
+    verbs = []
+    for word in range(0, len(pos_tags)):
+        if pos_tags[word][1].startswith('VB'):
+            verbs += [pos_tags[word][0]]
+    return verbs
+
+
 # global variables for object, class and attribute names
 
 class_name = ""
 attribute_version = ""
 class_version = ""
 
+
 # generate test cases and append them to the file
-def generate(sentence, jsonObject):
+def generate(sentence, json_object):
     global f
     pos_tags = get_pos(sentence)
 
     printable_sentence = sentence
 
-    givenJSON = [] if jsonObject['scenarios']['given'] == {} else jsonObject['scenarios']['given']
-    whenJSON = [] if jsonObject['scenarios']['when'] == {} else jsonObject['scenarios']['when']
-    thenJSON = [] if jsonObject['scenarios']['then'] == {} else jsonObject['scenarios']['then']
+    givenJSON = [] if json_object['scenarios']['given'] == {} else json_object['scenarios']['given']
+    whenJSON = [] if json_object['scenarios']['when'] == {} else json_object['scenarios']['when']
+    thenJSON = [] if json_object['scenarios']['then'] == {} else json_object['scenarios']['then']
     # Given step
 
     if "Given" in sentence:
-
-        nouns = []
-        values = []
-
         quoted_values = []
         quotes = get_quoted(sentence)
-
-        # handle quoted values: "<number>" will become number
+        # TODO:handle quoted values: "<number>" will become number
         for quote in quotes:
             if '>' in quote:
                 quote = quote.replace('>', '').replace('<', '')
@@ -85,93 +110,60 @@ def generate(sentence, jsonObject):
             quoted_values += [quote]
 
         # extract nouns and cardinals
-        word: int
-        for word in range(0, len(pos_tags) - 1):
-            if pos_tags[word][1] == "NN":
-                if pos_tags[word + 1][1] == "NN":
-                    nouns += [pos_tags[word][0] + " " + pos_tags[word + 1][0]]
-                    word += 1
-                else:
-                    nouns += [pos_tags[word][0]]
-            if pos_tags[word][1] == "CD":
-                values += [pos_tags[word][0]]
-        if (pos_tags[len(pos_tags) - 1][1] == "NN") and pos_tags[len(pos_tags) - 1][0] not in nouns:
-            nouns += [pos_tags[len(pos_tags) - 1][0]]
+        nouns, values = extract_nouns_and_values(pos_tags)
 
         # extract numbers
         number = [int(s) for s in sentence.split() if s.isdigit()]
-
-        # start output
-        f.write("@given('" + printable_sentence + "') \n")
-        f.write("def step_impl(context): \n")
+        print(f"nouns: {nouns}")
+        print(f"numbers: {number}")
 
         # put analysis into json object
-        givenJSON.append({"description": printable_sentence, "analysis": [pos_tags]})
-
+        info = {'nouns': nouns, 'numbers': number, 'parameters': values}
+        givenJSON.append({"description": printable_sentence, "analysis": [info]})
 
         # extract object, class and attribute names - global to be able to access for next steps as well
-        global attribute_version, class_version, class_name
-        print(f"attribute_version: {attribute_version}, class_v: {class_version}, class_name: {class_name}")
-
-        if len(nouns) > 0:
-            class_name = nouns[0]
-
-        if " " in class_name:
-            attribute_version = class_name.replace(" ", "_")
-            split = class_name.split(" ")
-            class_version = split[0].capitalize() + split[1].capitalize()
-        else:
-            attribute_version = class_name
-            class_version = class_name.capitalize()
-
-        print(f"attribute_version: {attribute_version}, class_v: {class_version}, class_name: {class_name}")
         # output to file
-        f.write("    context." + attribute_version + " = " + class_version + "(")
-
+        # f.write("    context." + attribute_version + " = " + class_version + "(")
         # add numerical values
-
-        if len(number) == 1:
-            f.write(str(number[0]) + ")\n")
-        elif len(number) > 1:
-            for word in range(0, len(number) - 1):
-                f.write(str(number[word]) + ",")
-            f.write(str(number[len(number) - 1]))
-            f.write(")\n")
-
-        # add quoted values
-
-        elif len(quoted_values) == 1:
-            f.write(quoted_values[0] + ")\n")
-        elif len(quoted_values) > 1:
-            for word in range(0, len(quoted_values) - 1):
-                f.write(quoted_values[word] + ",")
-            f.write(quoted_values[len(quoted_values) - 1])
-            f.write(")\n")
-
-        # add any other values
-
-        else:
-            if len(values) > 1:
-                for word in range(0, len(values) - 1):
-                    f.write(values[word] + ",")
-                f.write(values[len(values) - 1])
-                f.write(")\n")
-            else:
-                f.write(")\n")
-        f.write("\n")
-
-
+        #
+        # if len(number) == 1:
+        #     f.write(str(number[0]) + ")\n")
+        # elif len(number) > 1:
+        #     for word in range(0, len(number) - 1):
+        #         f.write(str(number[word]) + ",")
+        #     f.write(str(number[len(number) - 1]))
+        #     f.write(")\n")
+        #
+        # # add quoted values
+        #
+        # elif len(quoted_values) == 1:
+        #     f.write(quoted_values[0] + ")\n")
+        # elif len(quoted_values) > 1:
+        #     for word in range(0, len(quoted_values) - 1):
+        #         f.write(quoted_values[word] + ",")
+        #     f.write(quoted_values[len(quoted_values) - 1])
+        #     f.write(")\n")
+        #
+        # # add any other values
+        #
+        # else:
+        #     if len(values) > 1:
+        #         for word in range(0, len(values) - 1):
+        #             f.write(values[word] + ",")
+        #         f.write(values[len(values) - 1])
+        #         f.write(")\n")
+        #     else:
+        #         f.write(")\n")
+        # f.write("\n")
 
     # When step
-
     if "When" in sentence:
 
         # extract numbers
-
         number_values = [int(s) for s in sentence.split() if s.isdigit()]
+        nouns, values = extract_nouns_and_values(pos_tags)
 
-        # extract and handle quoted values
-
+        # TODO: extract and handle quoted values
         quoted_values = []
         quotes = get_quoted(sentence)
         for quote in quotes:
@@ -186,8 +178,11 @@ def generate(sentence, jsonObject):
             sentence=sentence
         )
 
-        verb = ""
+        verbs = extract_verbs(pos_tags)
+        print(verbs)
         description = ""
+        for verb in verbs:
+            print(srl_sentence['verb'][verb])
 
         if len(srl_sentence['verbs']) > 0:
 
@@ -195,6 +190,7 @@ def generate(sentence, jsonObject):
             verb = srl_sentence['verbs'][0]['verb']
             description = srl_sentence['verbs'][0]['description']
 
+            print(f"verbs: {verb} or {srl_sentence['verbs']}")
             arguments = []
 
             # replace useless characters
@@ -203,7 +199,6 @@ def generate(sentence, jsonObject):
             # go through list of arguments and extract values, nouns, compounds - basically useful info from arguments
             for arg in description:
                 value = arg.split(':')
-                print(value)
                 role = ""
                 if len(value) > 1:
                     role = value[0].strip()
@@ -235,7 +230,6 @@ def generate(sentence, jsonObject):
                             arguments += [value.strip()]
 
             # # create list of function arguments, including information from srl arguments and quoted values
-
             function_arguments = ""
 
             for val in arguments:
@@ -257,6 +251,7 @@ def generate(sentence, jsonObject):
                 if '"' not in quote and quote not in function_arguments:
                     function_arguments += quote + ", "
 
+            print(f"args:{function_arguments}")
             # create output
             whenJSON.append({"description": printable_sentence, "analysis": [pos_tags, [verb, description]]})
             # TODO: change this with own component
@@ -312,7 +307,6 @@ def generate(sentence, jsonObject):
             sentence=sentence
         )
 
-
         for quote in quotes:
             if '>' in quote:
                 quote = quote.replace('>', '').replace('<', '')
@@ -336,7 +330,6 @@ def generate(sentence, jsonObject):
                 # extract useful info from arguments - nouns, compounds, etc.
                 for arg in description:
                     value = arg.split(':')
-                    print(value)
                     if len(value) > 1:
                         role = value[0].strip()
                         value = value[1]
@@ -439,11 +432,10 @@ def generate(sentence, jsonObject):
 
             f.write("\n")
 
-    print("Done!")
-    jsonObject["scenarios"]["given"] = givenJSON
-    jsonObject["scenarios"]["when"] = whenJSON
-    jsonObject["scenarios"]["then"] = thenJSON
-    return jsonObject
+    json_object["scenarios"]["given"] = givenJSON
+    json_object["scenarios"]["when"] = whenJSON
+    json_object["scenarios"]["then"] = thenJSON
+    return json_object
 
 
 # generate tests for project
@@ -451,18 +443,17 @@ def generate(sentence, jsonObject):
 # TODO: add support for multiple files to be processed at once
 def generate_for_project(path):
     print(os.path.basename(path))
-    completeNLP2JSON = { # Dictionary to hold JSON data for output of NLP processing of all feature files
+    completeNLP2JSON = {  # Dictionary to hold JSON data for output of NLP processing of all feature files
         "files": []
     }
-    file2JSON = { # Dictionary to hold NLP processing results of 1 file
-            "name": os.path.basename(path),
-            "scenarios": {
-                "given": {},
-                "when": {},
-                "then": {}
-            }
+    file2JSON = {  # Dictionary to hold NLP processing results of 1 file
+        "name": os.path.basename(path),
+        "scenarios": {
+            "given": {},
+            "when": {},
+            "then": {}
         }
-
+    }
 
     # parse Gherkin scenarios
     documents = process_file(path)
@@ -474,6 +465,7 @@ def generate_for_project(path):
     # dump result to json file
     with open("nlp_results.json", "w") as write_file:
         json.dump(completeNLP2JSON, write_file, indent=2)
+
 
 for i in range(0, len(paths)):
     f = open("generated_code/generated_steps_" + str(i) + ".py", "a+")
